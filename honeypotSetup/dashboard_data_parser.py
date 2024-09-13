8337b0cac93e0479bfce47559963a96a69f0ed95
ottojonas <ottobjonas@outlook.com>
2024-09-13 16:09:15 +0100

    time to debug

--- honeypotSetup/dashboard_data_parser.py
+++ honeypotSetup/dashboard_data_parser.py
@@ -1,9 +1,11 @@
-import pandas as pd
 import re
+
+import pandas as pd
 import requests
 from icecream import ic
 
 
+# parser for the creds file | returns ip address, username and password
 def parse_creds_audits_log(creds_audits_log_file):
     data = []
 
@@ -19,36 +21,46 @@ def parse_creds_audits_log(creds_audits_log_file):
     return df
 
 
+# parse commands entered during ssh session
 def parse_cmd_audits_log(cmd_audits_log_file):
     data = []
 
     with open(cmd_audits_log_file, "r") as file:
         for line in file:
             lines = line.strip().split("\n")
-        pattern = re.compile(r"Command b'([^']*)'executed by (\d+\.\d+\.\d+\.\d+)")
 
-        for line in lines:
-            match = pattern.search(line)
-            if match:
-                command, ip = match.groups()
-                data.append({"IP Address": ip, "Command": command})
+            # regular expression to extract ip address and command
+            pattern = re.compile(r"Command b'([^']*)'executed by (\d+\.\d+\.\d+\.\d+)")
+
+            for line in lines:
+                match = pattern.search(line)
+                if match:
+                    command, ip = match.groups()
+                    data.append({"IP Address": ip, "Command": command})
 
     df = pd.DataFrame(data)
 
     return df
 
 
+# generate top 10 values from a df | supply column name | countrs how ofen each value occurs | stores in count column then returns to df with value / count
 def top_10_calculator(dataframe, column):
+    top_10_df = pd.DataFrame()
     for col in dataframe.columns:
         if col == column:
             top_10_df = dataframe[column].value_counts().reset_index().head(10)
             top_10_df.columns = [column, "count"]
+            break
+    ic(top_10_df)
     return top_10_df
 
 
+# takes ip as string type and uses cleantalk API to generate geolocation
 def get_country_code(ip):
 
     data_list = []
+
+    # api calls limited to 1000 every 60 seconds
     url = f"https://api.cleantalk.org/?method_name=ip_info&ip={ip}"
 
     try:
@@ -76,6 +88,7 @@ def get_country_code(ip):
     return data_list
 
 
+# takes a df with the ip addresses | converts each ip address to country geolocation code
 def ip_to_country_code(dataframe):
 
     data = []
@@ -86,4 +99,5 @@ def ip_to_country_code(dataframe):
         data.append({"IP Address": ip, "Country_Code:": parse_get_country})
 
     df = pd.DataFrame(data)
+    ic(df)
     return df
--- honeypotSetup/ssh_honeypot.py
+++ honeypotSetup/ssh_honeypot.py
@@ -1,24 +1,38 @@
+import argparse
+import getpass
 import logging
-from logging.handlers import RotatingFileHandler
 import socket
-import paramiko
 import threading
 import time
+from logging.handlers import RotatingFileHandler
 from pathlib import Path
+
+import paramiko
 from icecream import ic
 
+# Constants
 SSH_BANNER = "SSH-2.0-MySSHServer_1.0"
+
+# get base dir of where user is running honeypy from
 base_dir = base_dir = Path(__file__).parent.parent
-server_key = base_dir / "ssh_honeypot" / "static" / "server_key"
+
+# source creds_audits.log and cmd_audits.log file path
+server_key = base_dir / "honeypotSetup" / "static" / "server.key"
 creds_audits_log_local_file_path = (
-    base_dir / "ssh_honeypot" / "log_files" / "creds_audits.log"
+    base_dir / "honeypotSetup" / "log_files" / "creds_audits.log"
 )
 cmd_audits_log_local_file_path = (
-    base_dir / "ssh_honeypot" / "log_files" / "cmd_audits.log"
+    base_dir / "honeypotSetup" / "log_files" / "cmd_audits.log"
 )
-host_key = paramiko.RSAKey(filename=server_key)
+
+# ssh server host key
+private_key_password = getpass.getpass(prompt="enter password for server.key: ")
+host_key = paramiko.RSAKey(filename=server_key, password=private_key_password)
+
+# logging format
 logging_format = logging.Formatter("%(message)s")
 
+# funnel (catch all) logger
 funnel_logger = logging.getLogger("FunnelLogger")
 funnel_logger.setLevel(logging.INFO)
 funnel_handler = RotatingFileHandler(
@@ -26,6 +40,8 @@ funnel_handler = RotatingFileHandler(
 )
 funnel_handler.setFormatter(logging_format)
 funnel_logger.addHandler(funnel_handler)
+
+# credentials logger. capture ip addr, username and password
 creds_logger = logging.getLogger("CredsLogger")
 creds_logger.setLevel(logging.INFO)
 creds_handler = RotatingFileHandler(
@@ -89,6 +105,51 @@ def emulated_shell(channel, client_ip):
             channel.close()
         command += char
 
+        # emulate common shell commands
+        if char == b"\r":
+            if command.strip() == b"exit":
+                response = b"\n goodbye\n"
+                channel.close()
+
+            elif command.strip() == b"pwd":
+                response = b"\n" + b"\\usr\\local" + b"\r\n"
+                funnel_logger.info(
+                    f"command {command.strip()}" + "executed by" f"{client_ip}"
+                )
+
+            elif command.strip() == b"whoami":
+                response = b"\n" + b"corpuser1" + b"\r\n"
+                funnel_logger.info(
+                    f"Command {command.strip()}" + "executed by " f"{client_ip}"
+                )
+
+            elif command.strip() == b"ls":
+                response = b"\n" + b"jumpbox1.conf" + b"\r\n"
+                funnel_logger.info(
+                    f"Command {command.strip()}" + "executed by " f"{client_ip}"
+                )
+            elif command.strip() == b"ll":
+                response = b"\n" + b"jumpbox1.conf" + b"\r\n"
+                funnel_logger.info(
+                    f"Command {command.strip()}" + "executed by " f"{client_ip}"
+                )
+
+            elif command.strip() == b"cat jumpbox1.conf":
+                response = b"\n" + b"Go to deeboodah.com" + b"\r\n"
+                funnel_logger.info(
+                    f"Command {command.strip()}" + "executed by " f"{client_ip}"
+                )
+
+            else:
+                response = b"\n" + bytes(command.strip()) + b"\r\n"
+                funnel_logger.info(
+                    f"Command {command.strip()}" + "executed by " f"{client_ip}"
+                )
+
+            channel.send(response)
+            channel.send(b"corporate-jumpbox2$ ")
+            command = b""
+
 
 def client_handle(client, addr, username, password, tarpit=False):
     client_ip = addr[0]
@@ -112,18 +173,32 @@ def client_handle(client, addr, username, password, tarpit=False):
         standard_banner = "welcome"
 
         try:
+            # endless banner -> if tarpit option is passed then send 'endless' ssh banner
             if tarpit:
                 endless_banner = standard_banner * 100
                 for char in endless_banner:
                     channel.send(char)
                     time.sleep(8)
+            # standard banner -> send generic welcome banner to impersonate server
             else:
                 channel.send(standard_banner)
+
+            # send channel connection to emulated shell for interpritation
             emulated_shell(channel, client_ip=client_ip)
         except Exception as error:
             ic(error)
+    # generic catch all exception error code
     except Exception as error:
         ic(error)
+
+    # once session complete -> close transportation connection
+    finally:
+        try:
+            transport.close()
+        except Exception:
+            pass
+
+        client.close()
 
 
 def honeypot(address, port, username, password, tarpit=False):
