3eb8ad7ce1dbcaf58b1dfa53e6c04113d5a0f10a
ottojonas <ottobjonas@icloud.com>
2024-09-24 09:12:12 +0100

    aes

--- basicCryptography/AES/AESProject/obj/Debug/net8.0/AESProject.AssemblyInfo.cs
+++ basicCryptography/AES/AESProject/obj/Debug/net8.0/AESProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AESProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8ef899ac7046db34b1764b75a474682f091b62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07bbf0568d2ab6cf69640dfb0f5e171aec7653c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AESProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AESProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
